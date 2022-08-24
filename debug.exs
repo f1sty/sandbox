f689561778c3d9c455689f20c28da68dd530d43e
Yurii Skrynnykov <truef1s7@gmail.com>
2022-08-24 10:56:29 +0300

    Remove junk.

--- debug.exs
+++
@@ -1,8 +0,0 @@
-features = %{test: 1.2, name: "Koala", rank: :fool}
-names = ~w[John Alex Sean Andrew Lucas Booo]
-
-dbg(features)
-dbg([names | "Jacob"])
-dbg(["Jacob" | names])
-
-dbg(__ENV__.file |> Path.basename() |> File.exists?())
